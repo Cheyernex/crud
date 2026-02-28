088b77ee34f7afd2d6c69e8816f29dfe06262d82
Cheyernex <cheyernex@gmail.com>
2026-02-28 12:57:11 -0400

    feat: Add initial user entities and configure JPA, Lombok, and validation dependencies.

---
+++ src/main/java/com/example/crud/entity/Usuario.java
@@ -0,0 +1,20 @@
+package com.example.crud.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+}
